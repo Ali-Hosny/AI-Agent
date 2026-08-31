3a45abae7481896b7258901eee96bba7f81508ff
Ali Hosny <alihosny7ul@gmail.com>
2026-08-31 17:13:13 +0300

    Implemented a test response

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 
 
 
 def main():
     print("Hello from ai-agent!")
+
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
     if not api_key:
         raise RuntimeError("OPENROUTER_API_KEY not found in environment variables.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+
+    
+    )
+
+    print(f"Response: {response.choices[0].message.content}")
 
 if __name__ == "__main__":
     main()
