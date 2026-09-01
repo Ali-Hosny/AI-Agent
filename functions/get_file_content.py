f6db7b0d2e8accfec143ab700420e2d2825266ce
Ali Hosny <alihosny7ul@gmail.com>
2026-09-01 22:52:05 +0300

    Implemented and tested get_file_content which returns at most the first 10,000 characters in a file.

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    working_directory_absolute = os.path.abspath(working_directory)
+    target_file = os.path.normpath(os.path.join(working_directory_absolute, file_path))
+    is_valid_target = os.path.commonpath([working_directory_absolute, target_file]) == working_directory_absolute
+
+    if not is_valid_target:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(target_file):
+        return f'Error: file is not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(target_file, "r") as file:
+            content_string = file.read(MAX_CHARS)
+
+            if file.read(1):
+                content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+            return content_string
+    except Exception as e:
+        return f"Error: {str(e)}"
