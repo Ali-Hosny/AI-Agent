9c17b99cd5693f2ee8b1be04b542c9cb96158431
Ali Hosny <alihosny7ul@gmail.com>
2026-09-01 22:35:06 +0300

    Modified get_files_info, now it lists the name and size of each file in a passed directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -13,4 +13,23 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     if not os.path.isdir(target_directory):
         return f'Error: "{directory}" is not a directory'
 
-    return f'Success: "{directory}" is within the working directory'
+    # return f'Success: "{directory}" is within the working directory'
+
+    content: list[str] = []
+    try:
+        for file in os.listdir(target_directory):
+            name: str = file
+            size: int = os.path.getsize(os.path.join(target_directory, file))
+            is_dir:bool = os.path.isdir(os.path.join(target_directory, file))
+            content.append(f"- {file}: file_size={size} bytes, is_dir={is_dir}")
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+    result: str = "\n".join(content)
+
+    if directory != ".":
+        return f"Result for '{directory}' directory:\n" + result
+    
+    return "Result for current directory:\n" + result
+
+
